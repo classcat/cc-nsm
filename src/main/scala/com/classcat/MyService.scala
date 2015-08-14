aa2644dcc95eb45cfeb3cdd773550b3d492a979b
ClassCat <support@classcat.com>
2015-08-14 23:54:18 +0900

    init

---
+++ src/main/scala/com/classcat/MyService.scala
@@ -0,0 +1,82 @@
+package com.example
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+// we don't implement our route structure directly in the service actor because
+// we want to be able to test it independently, without having to spin up an actor
+class MyServiceActor extends Actor with defaultService with MyService {
+
+  // the HttpService trait defines only one abstract member, which
+  // connects the services environment to the enclosing actor or test
+  def actorRefFactory = context
+
+  // this actor only runs our route, but you could add
+  // other things here, like request stream processing
+  // or timeout handling
+  def receive = runRoute(defaultRoute ~ myRoute)
+}
+
+
+// this trait defines our service behavior independently from the service actor
+trait defaultService extends HttpService {
+
+    val defaultRoute =
+      path("") {
+        get {
+          respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+            complete {
+              <html>
+                <body>
+                  <h1>Say Default to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+                </body>
+              </html>
+            }
+          }
+        }
+    } ~
+    path("template") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+              html.index.render("にゃおちゃん").body
+          }
+        }
+      }
+  }
+
+}
+
+trait MyService extends HttpService {
+
+  val myRoute =
+    path("hello") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+  } ~
+    path("world") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Say World to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    }
+
+}
